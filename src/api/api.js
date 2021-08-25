a5cf16e15df369c2c9f3941582821f738b96d0ec
iqbaliamal <iqbaliamal@gmail.com>
2021-08-25 21:53:32 +0700

    base url api

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'https://backend.bantugerak.my.id/api'
 })
 
 export default Api
